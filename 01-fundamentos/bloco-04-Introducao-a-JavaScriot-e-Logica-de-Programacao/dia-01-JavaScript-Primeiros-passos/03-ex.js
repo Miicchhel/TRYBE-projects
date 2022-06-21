51e8dddbee17f6c264208cc4b027a6904be4a8d6
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-06-21 13:26:21 -0300

    exportando a variável resultado

--- 01-fundamentos/bloco-04-Introducao-a-JavaScriot-e-Logica-de-Programacao/dia-01-JavaScript-Primeiros-passos/03-ex.js
+++ 01-fundamentos/bloco-04-Introducao-a-JavaScriot-e-Logica-de-Programacao/dia-01-JavaScript-Primeiros-passos/03-ex.js
@@ -1,9 +1,15 @@
-const nota = 80;
+const nota = 40;
+let resultado;
 
 if(nota >= 80){
-  console.log("Parabéns, você foi aprovada(o)!");
+  resultado = 'aprovada';
+  // console.log("Parabéns, você foi aprovada(o)!");
 }else if(nota >= 60 && nota < 80){
-  console.log("Você está na nossa lista de espera.");
+  resultado = 'lista';
+  // console.log("Você está na nossa lista de espera.");
 }else{
-  console.log("Você foi reprovada(o).")
+  resultado = 'reprovada';
+  // console.log("Você foi reprovada(o).");
 }
+
+export {resultado};

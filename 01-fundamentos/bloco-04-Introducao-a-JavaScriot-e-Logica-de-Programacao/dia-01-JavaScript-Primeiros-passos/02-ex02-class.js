fd39a3a255dd1673e9df5a05c2aa2fc686f9ef2f
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-06-21 12:03:07 -0300

    Exercicio aula: "tipos primitivos, tipagem dinâmica e operações aritméticas"

---
+++ 01-fundamentos/bloco-04-Introducao-a-JavaScriot-e-Logica-de-Programacao/dia-01-JavaScript-Primeiros-passos/02-ex02-class.js
@@ -0,0 +1,9 @@
+const base = 5;
+let height = 8; 
+const area = base * height;
+const perimeter = 2*base + 2*height
+
+console.log(`base = ${base}m;
+height = ${height}m
+area = ${area}m²;
+perimeter = ${perimeter}m;`);

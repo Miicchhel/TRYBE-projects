2299a1704205590ae21e4258833b661472539b3f
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-06-26 17:57:21 -0300

    maior primo entre 2 e n

---
+++ 01-fundamentos/bloco-04-Introducao-a-JavaScriot-e-Logica-de-Programacao/dia-03-Logica-de-programacao-e-algoritmos/exerDia/Q4.js
@@ -0,0 +1,18 @@
+let n = 50
+maior = 0
+
+for (let i = 2; i <= n; i++) {
+  let cont = 0;
+  for(let j = 1; j <= i; j++){
+    if (i % j === 0){
+      cont += 1;
+      if(cont > 2){
+        break
+      }
+    }
+  }
+  if(cont === 2){
+    maior = i;
+  }
+}
+console.log(maior);

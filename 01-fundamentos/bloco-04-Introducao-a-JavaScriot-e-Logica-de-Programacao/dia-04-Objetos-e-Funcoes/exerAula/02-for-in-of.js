8602f6c96d2135c8520df5ac0c337675cd2877b4
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-06-26 16:27:46 -0300

    renomeando pasta para dia04

---
+++ 01-fundamentos/bloco-04-Introducao-a-JavaScriot-e-Logica-de-Programacao/dia-04-Objetos-e-Funcoes/exerAula/02-for-in-of.js
@@ -0,0 +1,19 @@
+//01-
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for(let i in names){
+  console.log(`Olá ${names[i]}`);
+}
+
+//02-
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for ()

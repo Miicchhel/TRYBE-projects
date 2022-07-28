56228ec66495014c60522f24748c2b37c1b7bfb8
Michel Ferreira da Silva Melo <michel.ferreira.melo@gmail.com>
2022-07-27 21:50:18 -0300

    Delete 01-fundamentos/bloco-08-HigherOrderFunctions-JavaScript-ES6/dia-02-HOF-ForEach-find-some-every-sort.js directory

--- 01-fundamentos/bloco-08-HigherOrderFunctions-JavaScript-ES6/dia-02-HOF-ForEach-find-some-every-sort.js/Q4.js
+++
@@ -1,7 +0,0 @@
-const books = require('./books');
-
-function booksOrderedByReleaseYearDesc() {
-  return books.sort((a, b) => a.releaseYear - b.releaseYear);
-}
-booksOrderedByReleaseYearDesc()
-console.log(books)
--- 01-fundamentos/bloco-08-HigherOrderFunctions-JavaScript-ES6/dia-02-HOF-ForEach-find-some-every-sort.js/Q6.js
+++
@@ -1,9 +0,0 @@
-const books = require('./books');
-
-const expectedResult = true;
-
-function someBookWasReleaseOnThe80s() {
-  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear < 1990);
-}
-
-console.log(someBookWasReleaseOnThe80s());
